90543d88ce0c980a4d2c5ec9b9ddfe5cea82eb09
unknown <18310343827@163.com>
2024-11-20 22:05:54 +0800

    chore: update daoyuanketang

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -53,9 +53,9 @@ export default function Resume() {
               position: "软件工程师",
               period: "2023.10 - 至今",
               responsibilities: [
-                "负责数字化平台的开发管理工作，卫星终端定位调度平台的软件设计开发和维护，天通智能调度平台，https://www.ctsat.com:60902。用户主要为各省应急厅和工信部等国家级应急单位。",
-                "内部系统全栈开发，管理卫星终端的合规性的管理平台提供市场部使用；为卫星研究院开发地球站北斗园区定位小程序以及辅助科研工作的小工具；设计并开发研发效能管理平台，简化内部审核流程以促进企业数字化转型。涉及的技术栈有 redis，mysql，java，vue，react，springboot，taro 等。",
-                "一些运维工作，服务器、网络打通、数据库的管理；使用电信研发云规范外包人员的 git 代码管理，配置项目的 CI/CD 使流程更加规范；使用中国电信自研的 ccse，telepg 等国产化 paas 组件替代 k8s，postgresql；使用 grafana，prometheus，skywalking 等工具搭建图表绘制和自动化监控报警系统。"
+                "负责数字化平台的开发管理工作，卫星终端定位调度平台的软件设计开发和维护，天通智能调度平台，https://www.ctsat.com:60902。用户主要为各省应急厅和工信部等国家级应急单位",
+                "内部系统全栈开发，管理卫星终端的合规性的管理平台提供市场部使用；为卫星研究院开发地球站北斗园区定位小程序以及辅助科研工作的小工具；设计并开发研发效能管理平台，简化内部审核流程以促进企业数字化转型。涉及的技术栈有 redis，mysql，java，vue，react，springboot，taro 等",
+                "一些运维工作，服务器、网络打通、数据库的管理；使用电信研发云规范外包人员的 git 代码管理，配置项目的 CI/CD 使流程更加规范；使用中国电信自研的 ccse，telepg 等国产化 paas 组件替代 k8s，postgresql；使用 grafana，prometheus，skywalking 等工具搭建图表绘制和自动化监控报警系统"
               ]
             },
             {
@@ -64,8 +64,8 @@ export default function Resume() {
               period: "2021.3 - 2023.10",
               responsibilities: [
                 "负责知乎社区功能的前端开发，主要负责知乎网站建设和 app 端的前端页面开发。包括，搜索页，回答页，文章页，首页，个人主页，直播功能等技术栈主要是 react",
-                "搜索模块的卡片展示，自研了卡片工厂系统，使得卡片可以灵活配置，极大地简化了开发流程，提高开发效率。",
-                "负责知乎客服系统的优化，升级重构了客服系统的前端技术栈和 UI 设计，提高了开发效率，优化了用户体验。"
+                "搜索模块的卡片展示，自研了卡片工厂系统，使得卡片可以灵活配置，极大地简化了开发流程，提高开发效率",
+                "负责知乎客服系统的优化，升级重构了客服系统的前端技术栈和 UI 设计，提高了开发效率，优化了用户体验"
               ]
             },
             {
@@ -73,7 +73,8 @@ export default function Resume() {
               position: "前端开发工程师",
               period: "2019.7 - 2021.3",
               responsibilities: [
-                "从 0 到 1 开发了多个项目，包括官网、管理后台、小程序等，技术栈为 vue，uni-app"
+                "道远课堂官网 daoyuanketang.com 从 0 到 1 开发，包括课程播放，课后习题，课程购买支付，伴读，后台管理等系统，技术栈为 vue，express，nodejs，socket.io，element-ui 等",
+                "刷题小程序以及日常宣传 h5 海报的开发，技术栈为 uniapp"
               ]
             },
             {
@@ -101,7 +102,7 @@ export default function Resume() {
           <h2 className="text-2xl font-semibold text-gray-700 mb-4">其他项目经验</h2>
           <div className="bg-white shadow rounded-lg p-6">
             <ul className="list-disc list-inside text-gray-700">
-              <li>使用 taro + python fastapi + supabse 开发微信小程序 “闭气练习” 以供自由潜水员日常训练使用，可通过微信小程序搜索，目前用户量几百个，日活几十个</li>
+              <li>使用 taro + python fastapi + supabse 开发微信小程序 “闭气练习” 供自由潜水员日常训练使用，可通过微信小程序搜索，目前用户量几百，日活几十</li>
               <li>使用 react native 利用 ai 画图开发小游戏 “龙虎斗”，并开源到 github 主页，可访问<a href="https://github.com/zxyfreediver/react-native-game" target="_blank" rel="noopener noreferrer"> 我的 github 主页 </a>查看，点击链接下载体验 <a href="https://expo.dev/accounts/zhaoxingyu93/projects/dragon-tiger-fight" target="_blank" rel="noopener noreferrer">https://expo.dev/accounts/zhaoxingyu93/projects/dragon-tiger-fight</a></li>
               <li>使用 nextjs + vercel + cursor 部署自己的个人网站</li>
             </ul>
